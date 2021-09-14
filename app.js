b23e6810148880c02f62aa282b629b1b48a0c290
Hubert Mężyk <85291840+Belkowski656@users.noreply.github.com>
2021-09-14 20:09:10 +0200

    deploy

---
+++ app.js
@@ -0,0 +1,20 @@
+const hamburger = document.querySelector(".hamburger");
+const menuElements = document.querySelectorAll(".nav__link");
+const logo = document.querySelector(".nav__img");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  document.querySelector(".nav__menu").classList.toggle("active");
+});
+
+menuElements.forEach((element) => {
+  element.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    document.querySelector(".nav__menu").classList.remove("active");
+  });
+});
+
+logo.addEventListener("click", () => {
+  hamburger.classList.remove("active");
+  document.querySelector(".nav__menu").classList.remove("active");
+});
